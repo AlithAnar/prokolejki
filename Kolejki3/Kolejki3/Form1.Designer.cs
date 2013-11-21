a69a285cd1b3d73646592f9e4c9ff1443e2bc703
Grzegorz Pasieka <alith.anar.ensiferum@gmail.com>
2013-11-22 00:03:18 +0100

    szkic GUI

---
+++ Kolejki3/Kolejki3/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Kolejki3
+    {
+    partial class Form1
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLambda = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownModules = new System.Windows.Forms.NumericUpDown();
+            this.akcjeBox = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxTypes = new System.Windows.Forms.ComboBox();
+            this.numericUpDownQueueSize = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownMachineCount = new System.Windows.Forms.NumericUpDown();
+            this.textBoxMi = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszUstawieniaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wczytajUstawieniaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownModules)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQueueSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMachineCount)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "λ";
+            // 
+            // textBoxLambda
+            // 
+            this.textBoxLambda.Location = new System.Drawing.Point(54, 69);
+            this.textBoxLambda.Name = "textBoxLambda";
+            this.textBoxLambda.Size = new System.Drawing.Size(41, 20);
+            this.textBoxLambda.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "moduly:";
+            // 
+            // numericUpDownModules
+            // 
+            this.numericUpDownModules.Location = new System.Drawing.Point(54, 107);
+            this.numericUpDownModules.Name = "numericUpDownModules";
+            this.numericUpDownModules.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDownModules.TabIndex = 3;
+            // 
+            // akcjeBox
+            // 
+            this.akcjeBox.FormattingEnabled = true;
+            this.akcjeBox.Location = new System.Drawing.Point(568, 220);
+            this.akcjeBox.Name = "akcjeBox";
+            this.akcjeBox.Size = new System.Drawing.Size(203, 342);
+            this.akcjeBox.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.textBoxMi);
+            this.panel1.Controls.Add(this.numericUpDownMachineCount);
+            this.panel1.Controls.Add(this.numericUpDownQueueSize);
+            this.panel1.Controls.Add(this.comboBoxTypes);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(130, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(405, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Typ:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Długość kolejki:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(222, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Ilość maszyn:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(273, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "M:";
+            // 
+            // comboBoxTypes
+            // 
+            this.comboBoxTypes.FormattingEnabled = true;
+            this.comboBoxTypes.Location = new System.Drawing.Point(92, 13);
+            this.comboBoxTypes.Name = "comboBoxTypes";
+            this.comboBoxTypes.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxTypes.TabIndex = 4;
+            // 
+            // numericUpDownQueueSize
+            // 
+            this.numericUpDownQueueSize.Location = new System.Drawing.Point(93, 49);
+            this.numericUpDownQueueSize.Name = "numericUpDownQueueSize";
+            this.numericUpDownQueueSize.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownQueueSize.TabIndex = 5;
+            // 
+            // numericUpDownMachineCount
+            // 
+            this.numericUpDownMachineCount.Location = new System.Drawing.Point(298, 11);
+            this.numericUpDownMachineCount.Name = "numericUpDownMachineCount";
+            this.numericUpDownMachineCount.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownMachineCount.TabIndex = 6;
+            // 
+            // textBoxMi
+            // 
+            this.textBoxMi.Location = new System.Drawing.Point(298, 48);
+            this.textBoxMi.Name = "textBoxMi";
+            this.textBoxMi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMi.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.numericUpDown4);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(130, 134);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(404, 49);
+            this.panel2.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Ilość połączeń:";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(91, 17);
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(783, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zapiszUstawieniaToolStripMenuItem,
+            this.wczytajUstawieniaToolStripMenuItem,
+            this.zakończToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // zapiszUstawieniaToolStripMenuItem
+            // 
+            this.zapiszUstawieniaToolStripMenuItem.Name = "zapiszUstawieniaToolStripMenuItem";
+            this.zapiszUstawieniaToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.zapiszUstawieniaToolStripMenuItem.Text = "Zapisz ustawienia";
+            // 
+            // wczytajUstawieniaToolStripMenuItem
+            // 
+            this.wczytajUstawieniaToolStripMenuItem.Name = "wczytajUstawieniaToolStripMenuItem";
+            this.wczytajUstawieniaToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.wczytajUstawieniaToolStripMenuItem.Text = "Wczytaj ustawienia";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Linen;
+            this.panel3.Location = new System.Drawing.Point(12, 216);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(537, 346);
+            this.panel3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Stwórz";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 580);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.akcjeBox);
+            this.Controls.Add(this.numericUpDownModules);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxLambda);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Kolejki";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownModules)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQueueSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMachineCount)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLambda;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownModules;
+        private System.Windows.Forms.ListBox akcjeBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxMi;
+        private System.Windows.Forms.NumericUpDown numericUpDownMachineCount;
+        private System.Windows.Forms.NumericUpDown numericUpDownQueueSize;
+        private System.Windows.Forms.ComboBox comboBoxTypes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszUstawieniaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wczytajUstawieniaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        }
+    }
+
