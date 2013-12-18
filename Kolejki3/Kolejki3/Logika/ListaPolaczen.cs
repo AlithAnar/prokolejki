205c2d899b94c823867cc74d6ba8fe02d1c5cffa
Grzegorz Pasieka <alith.anar.ensiferum@gmail.com>
2013-12-18 15:26:15 +0100

    brakujace klasy

---
+++ Kolejki3/Kolejki3/Logika/ListaPolaczen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejki3.Logika
+{
+    class ListaPolaczen : List <Polaczenie>
+    {
+        public ListaPolaczen (Polaczenie p)
+        {
+            this.Add(p);
+        }
+
+        public Polaczenie wylosujPolaczenie()
+        {
+            Random r = new Random();
+            foreach (Polaczenie pp in this)
+            {
+                if (pp.chance <= r.NextDouble())
+                    return pp;
+            }
+            return this[0];
+        }
+    }
+}
---
+++ Kolejki3/Kolejki3/Logika/Modul.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolejki3.Logika
+{
+    class Modul
+    {
+        static private int _ID = 0;
+        private ListaMaszyn maszyny;
+        private Kolejka bufor;
+        public ListaPolaczen polaczenia;
+        public int ID { get; set; }
+
+        public Modul (Maszyna m, Kolejka k)
+        {
+            maszyny = new ListaMaszyn();
+            maszyny.Add(m);
+            bufor = k;
+            ID = _ID;
+            _ID++;
+        }
+
+        public Modul(ListaMaszyn lm, Kolejka k)
+        {
+            maszyny = lm;
+            bufor = k;
+            _ID++;
+        }
+
+        public bool isFull()
+        {
+            if (bufor.isFull()) return true;
+            else return false;
+        }
+
+        public void putQueue(Zdarzenie z)
+        {
+            Console.Out.Write("modul " + ID + " ");
+            bufor.put(z);
+            if (maszyny.isFree())
+                maszyny.freeMachine().poloz(bufor.get());
+        }
+
+        public Zdarzenie outMachine(int nr)
+        {
+            Console.Out.Write("modul " + ID + " ");
+            Zdarzenie z = maszyny[nr].zdejmij();
+            return z;
+        }
+    }
+}
